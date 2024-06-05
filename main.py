97a7eab38fddd981a8f3b9c7bc1f5906b5f237c3
Learner.Link Admin <help@learner.link>
2024-06-05 14:55:20 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,67 @@
+# Import required packages
+from flask import *
+from menu import *
+
+app = Flask("__main__")
+app.secret_key = "LKjskejdv9w9er8"
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template("index.html")
+
+@app.route("/menu", methods=['GET'])
+def menu():
+    collection = request.args['collection']
+    return render_template("menu.html",
+                           foodMenu=foodMenu,
+                           collection=collection)
+
+
+@app.route("/customise", methods=['GET', "POST"])
+def customise():
+    pizza = request.args["pizza"]
+    return render_template("customise.html",
+                           sizeOptions=sizeOptions,
+                           sauceOptions=sauceOptions,
+                           baseOptions=baseOptions,
+                           pizzaDetails=foodMenu['toppingOptions'][pizza],
+                           pizza=pizza
+                           )
+
+
+@app.route("/cart", methods=["GET", "POST"])
+def cart():
+    if request.method == "GET":
+        return render_template("cart.html", order=session['order'], foodMenu=foodMenu)
+    else:
+        if 'order' not in session:
+            session['order'] = []
+        category = request.form['category']
+        if category == 'toppingOptions':
+            price = foodMenu['toppingOptions'][request.form['pizza']]['price'] + \
+                    sizeOptions[request.form['size']] + \
+                    baseOptions[request.form['base']] + \
+                    sauceOptions[request.form['sauce']]
+            session['order'].append([category,
+                                     request.form['pizza'],
+                                     request.form['size'],
+                                     request.form['base'],
+                                     request.form['sauce'],
+                                     price])
+        else:
+            item = request.form['item']
+            price = foodMenu[category][item]['price']
+            session['order'].append([category, item, price])
+        session.modified = True
+        print(session['order'])
+        return redirect(url_for('cart'))
+
+
+@app.route('/remove/<item>')
+def remove(item):
+    session['order'].pop(int(item))
+    session.modified = True
+    return redirect(url_for('cart'))
+
+app.run(debug=True)
---
+++ menu.py
@@ -0,0 +1,82 @@
+
+sizeOptions = {"New Yorker": 5.00,
+               "Large": 2.00,
+               "Small": 0.00}
+
+sauceOptions = {"BBQ": 0.0,
+                "Tomato": 0.0,
+                "Bechemal": 1.0,
+                "None": -1.0}
+
+baseOptions = {"Classic Crust":0.0,
+               "Deep Pan": 0.0,
+               "Thin 'n' Crispy":0.0,
+               "Gluten Free": 2.0}
+
+extraToppings = {"Cheese": 0.5,
+                 "Bacon": 0.5,
+                 "Caramelised Onion": 0.5,
+                 "None": 0.0}
+
+foodMenu = {'toppingOptions': {"Hawaiian": {"price": 10.0,
+                               "description": "A classic pairing of smoky leg ham & sweet pineapple pieces.",
+                               "kJ": 4384,
+                               "image": "hawaiian.png"
+                               },
+               "Meatlovers": {"price": 10.0,
+                               "description": "Mega loaded, mega tasty. Featuring seasoned chicken, smoked leg ham, seasoned ground beef, pepperoni slices, Italian sausage & crispy rasher bacon, brought together with a hickory BBQ sauce.",
+                               "kJ": 4000,
+                               "image": "meatlovers.png"
+                               },
+               "Butter Chicken": {"price": 12.0,
+                               "description": "Seasoned chicken with jalapeños, mushrooms, onion, capsicum and tomato on an Indian butter sauce base topped with a spiced Indian butter sauce swirl.",
+                               "kJ": 5384,
+                               "image": "ButterChicken.png"
+                               }
+                  },
+    'sideOptions': {"Garlic bread": {
+                        "title": "Garlic Bread",
+                        "price": 3.0,
+                        "kJ": 3204,
+                        "image": "vegan-garlic-bread-3.webp",
+                        "description": "A warm bun with garlic butter on it"
+                    },
+               "Vegan Cheesy Garlic Bread": {
+                        "title": "Vegan Cheesy Garlic Bread",
+                        "price": 5.0,
+                        "kJ": 3204,
+                        "image": "vegan-garlic-bread-3.webp",
+                        "description": "The same as the normal garlic bread but with margarine and a price increase"
+                    },
+               "Meatballs": {
+                        "title": "Meatballs",
+                        "price": 2.0,
+                        "kJ": 1234,
+                        "image": "meatballs.webp",
+                        "description": "Balls made of meat."
+                    },
+               "Spud fireballs": {
+                        "title": "Spud Fireballs",
+                        "price": 4.0,
+                        "kJ": 4321,
+                        "image": "potato-on-fire-260nw-509633.webp",
+                        "description": "Mashed potato mixed with jalepeno, dipped in spicy batter and deep fried."
+                    }
+                },
+    'drinkOptions': {"Coca-Cola": {
+                        "title": "Coca-Cola",
+                        "price": 4.0,
+                        "kJ": 4321,
+                        "image": "potato-on-fire-260nw-509633.webp",
+                        "description": "Water, Sugar, and other ingredients"
+                    },
+                    "Fanta": {
+                        "title": "Fanta",
+                        "price": 4.0,
+                        "kJ": 4323,
+                        "image": "potato-on-fire-260nw-509633.webp",
+                        "description": "Water, Sugar, and other ingredients"
+                    }
+                }
+
+}
